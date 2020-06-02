5ac0a5cdf62b0c8249c8c6c54c09c5dca304fe16
PotatoInDespair <dcc255@cornell.edu>
2020-06-02 15:56:26 -0400

    linting/formatting setup part 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,25 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "linebreak-style": 0,
-    "semi": 2,
-  }
+    // enforcing double quotes, semicolons, and trailing commas
+    quotes: [
+      "warn",
+      "double",
+      {
+        avoidEscape: true,
+      },
+    ],
+    semi: 2,
+    "comma-dangle": ["warn", "always-multiline"],
+  },
 };
